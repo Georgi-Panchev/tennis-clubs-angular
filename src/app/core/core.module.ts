aafa5e847535c5939d4a6b39b244e6046842f402
Georgi <GeorgiPanchev@mail.com>
2020-03-29 18:57:48 +0300

    Not Found Component Added

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,8 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 
+import { NotFoundComponent } from './components/not-found/not-found.component';
+
 @NgModule({
-    declarations: [],
+    declarations: [
+        NotFoundComponent
+    ],
     imports: [
         CommonModule,
         HttpClientModule
