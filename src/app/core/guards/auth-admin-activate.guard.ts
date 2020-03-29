0f4e0a2aeded269729a8437819b6a2e80aec9521
Georgi <GeorgiPanchev@mail.com>
2020-03-29 18:45:31 +0300

    Admin Guard Added

---
+++ src/app/core/guards/auth-admin-activate.guard.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { CanActivate } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AuthAdminActivate implements CanActivate {
+    // eslint-disable-next-line no-useless-constructor
+    constructor(private authService: AuthService) {}
+
+    canActivate(): boolean {
+        return this.authService.isUserAdmin();
+    }
+}
