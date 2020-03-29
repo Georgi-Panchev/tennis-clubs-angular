8d870e61f18b653e1c8d2a9c48dfc73a1f5c9b35
Georgi <GeorgiPanchev@mail.com>
2020-03-29 19:00:12 +0300

    Home Component Added

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 
+import { HomeComponent } from './components/home/home.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
     declarations: [
+        HomeComponent,
         NotFoundComponent
     ],
     imports: [
