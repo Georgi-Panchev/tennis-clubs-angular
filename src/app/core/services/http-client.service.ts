4941a6e02aa3c298d3a1ae4376f20bf45c925c1f
Georgi <GeorgiPanchev@mail.com>
2020-03-29 18:40:37 +0300

    Http Client Service Added

---
+++ src/app/core/services/http-client.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+const baseUrl = 'http://localhost:8000';
+
+@Injectable()
+export class HttpClientService {
+    // eslint-disable-next-line no-useless-constructor
+    constructor(private httpClient: HttpClient) {}
+
+    get(url) {
+        return this.httpClient.get(`${baseUrl}${url}`);
+    }
+
+    post(url, data) {
+        return this.httpClient.post(`${baseUrl}${url}`, data);
+    }
+
+    put(url, data) {
+        return this.httpClient.put(`${baseUrl}${url}`, data);
+    }
+
+    delete(url) {
+        return this.httpClient.delete(`${baseUrl}${url}`);
+    }
+}
