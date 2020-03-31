413ea9e2e347fb3395bc95354dff2835c0828abb
Georgi <GeorgiPanchev@mail.com>
2020-03-31 19:43:55 +0300

    User Register Component Logic Applied

--- src/app/users/user-register/user-register.component.ts
+++ src/app/users/user-register/user-register.component.ts
@@ -1,14 +1,44 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component, OnDestroy, ChangeDetectionStrategy } from '@angular/core';
 import { UserModel } from '../shared/user.model';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../store/app.state';
+import { UserActions } from '../../store/users/user.actions';
 
 @Component({
     selector: 'app-user-register',
-    templateUrl: './user-register.component.html'
+    templateUrl: './user-register.component.html',
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class UserRegisterComponent implements OnDestroy {
-    constructor() {}
+    private subscription: Subscription = new Subscription();
 
-    register(user: UserModel): void {}
+    // eslint-disable-next-line no-useless-constructor
+    constructor(
+        private router: Router,
+        private store: Store<AppState>,
+        private usersActions: UserActions
+    ) {}
 
-    ngOnDestroy(): void {}
+    register(user: UserModel): void {
+        console.log(user);
+        this.usersActions.register(user);
+
+        this.subscription.add(
+            this.store
+                .select((state: AppState) => state.users.isUserRegistered)
+                .subscribe((isUserRegistered: boolean) => {
+                    if (isUserRegistered) {
+                        this.router.navigateByUrl('users/login');
+                    }
+                })
+        );
+    }
+
+    ngOnDestroy(): void {
+        if (this.subscription) {
+            this.subscription.unsubscribe();
+        }
+    }
 }
