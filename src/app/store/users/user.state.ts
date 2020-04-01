6362aa43423253b00fd0ecf3e471d0e306186a6f
Georgi <GeorgiPanchev@mail.com>
2020-04-01 19:16:32 +0300

    Store User State Updated

--- src/app/store/users/user.state.ts
+++ src/app/store/users/user.state.ts
@@ -1,7 +1,21 @@
 export interface UserState {
     isUserRegistered: boolean;
+    isUserAuthenticated: boolean;
+    token: string;
+    user: {
+        userId: string;
+        username: string;
+        roles: string[];
+    };
 }
 
 export const initialState: UserState = {
-    isUserRegistered: false
+    isUserRegistered: false,
+    isUserAuthenticated: false,
+    token: null,
+    user: {
+        userId: null,
+        username: null,
+        roles: []
+    }
 };
