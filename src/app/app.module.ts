a3bff249675d7547264a694586f57026a2b855c1
Georgi <GeorgiPanchev@mail.com>
2020-04-01 19:56:56 +0300

    App Module Updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { CoreModule } from './core/core.module';
 import { ToastrModule } from 'ngx-toastr';
-import { UsersModule } from './users/users.module';
-
 import { AppRoutingModule } from './app-routing.module';
+import { CoreModule } from './core/core.module';
+
 import { AppComponent } from './app.component';
+
+import { StoreModule, Store } from '@ngrx/store';
+import { appReducers } from './store/app.reducers';
 
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { JwtInterceptor } from './core/interceptors/jwt.interceptor';
 import { ResponseHandlerInterceptor } from './core/interceptors/response-handler.interceptor';
 
-import { StoreModule } from '@ngrx/store';
-import { appReducers } from './store/app.reducers';
+import { AppState } from './store/app.state';
+import { AuthService } from './core/services/auth.service';
+import { LOGIN_USER } from './store/users/user.actions';
 
 @NgModule({
     declarations: [
@@ -23,10 +26,9 @@ import { appReducers } from './store/app.reducers';
         BrowserModule,
         BrowserAnimationsModule,
         ToastrModule.forRoot(),
-        AppRoutingModule,
         CoreModule,
-        StoreModule.forRoot(appReducers),
-        UsersModule
+        AppRoutingModule,
+        StoreModule.forRoot(appReducers)
     ],
     providers: [
         { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
@@ -34,4 +36,21 @@ import { appReducers } from './store/app.reducers';
     ],
     bootstrap: [ AppComponent ]
 })
-export class AppModule {}
+export class AppModule {
+    constructor(
+        private store: Store<AppState>,
+        private authService: AuthService
+    ) {
+        if (authService.isUserAuthenticated()) {
+            const payload = {
+                success: authService.isUserAuthenticated(),
+                token: authService.getToken(),
+                user: authService.getUser()
+            };
+            store.dispatch({
+                type: LOGIN_USER,
+                payload
+            });
+        }
+    }
+}
