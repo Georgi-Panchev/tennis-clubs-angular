52f346b34e118550dadeb41c3d6019c881786cc2
Georgi <GeorgiPanchev@mail.com>
2020-04-01 19:44:36 +0300

    Header Component Template Implemented

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,12 +4,14 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { HomeComponent } from './components/home/home.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
     declarations: [
         HomeComponent,
         NotFoundComponent,
+        HeaderComponent,
         FooterComponent
     ],
     imports: [
