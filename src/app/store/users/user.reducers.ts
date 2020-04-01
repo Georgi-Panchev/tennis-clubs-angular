041a139e2d44fdade25bd54412100e27ce0a2736
Georgi <GeorgiPanchev@mail.com>
2020-04-01 19:18:20 +0300

    Store User Reducers Updated

--- src/app/store/users/user.reducers.ts
+++ src/app/store/users/user.reducers.ts
@@ -1,5 +1,5 @@
 import { initialState, UserState } from './user.state';
-import { REGISTER_USER } from './user.actions';
+import { REGISTER_USER, LOGIN_USER } from './user.actions';
 
 function register(state, action) {
     const payload = action.payload;
@@ -8,10 +8,21 @@ function register(state, action) {
     });
 }
 
+function login(state, action) {
+    const payload = action.payload;
+    return Object.assign({}, state, {
+        isUserAuthenticated: payload.success,
+        token: payload.token,
+        user: payload.user ? payload.user : {}
+    });
+}
+
 export function userReducers(state: UserState = initialState, action) {
     switch (action.type) {
         case REGISTER_USER:
             return register(state, action);
+        case LOGIN_USER:
+            return login(state, action);
         default:
             return state;
     }
