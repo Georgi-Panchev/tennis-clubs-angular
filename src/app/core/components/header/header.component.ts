a8c5a473fc9d0976dcb980a56b9b7e6a70fac536
Georgi <GeorgiPanchev@mail.com>
2020-04-01 19:50:49 +0300

    Header Component Logic Implemented

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,7 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
+import { AppState } from '../../../store/app.state';
+import { Store } from '@ngrx/store';
+import { UserActions } from '../../../store/users/user.actions';
+import { AuthService } from '../../services/auth.service';
+import { Subscription } from 'rxjs';
+import { UserState } from '../../../store/users/user.state';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html'
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit, OnDestroy {
+    private subscription: Subscription = new Subscription();
+    isUserAuthenticated = false;
+    isUserAdmin = false;
+    username: string = null;
+
+    // eslint-disable-next-line no-useless-constructor
+    constructor(
+        private router: Router,
+        private store: Store<AppState>,
+        private userActions: UserActions,
+        private authService: AuthService
+    ) {}
+
+    ngOnInit(): void {
+        this.subscription.add(
+            this.store
+                .select((state: AppState) => state.users)
+                .subscribe((userState: UserState) => {
+                    this.isUserAuthenticated = userState.isUserAuthenticated;
+                    this.isUserAdmin = userState.user.roles.includes('Admin');
+                    this.username = userState.user.username;
+                })
+        );
+    }
+
+    logout() {
+        this.userActions.logout();
+
+        this.authService.deauthenticateUser();
+        this.authService.removeUser();
+        this.router.navigateByUrl('home');
+    }
+
+    ngOnDestroy(): void {
+        this.subscription.unsubscribe();
+    }
+}
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,23 +1,33 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+
+import { HttpClientService } from './services/http-client.service';
 
 import { HomeComponent } from './components/home/home.component';
-import { NotFoundComponent } from './components/not-found/not-found.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
     declarations: [
         HomeComponent,
-        NotFoundComponent,
         HeaderComponent,
-        FooterComponent
+        FooterComponent,
+        NotFoundComponent
     ],
     imports: [
         CommonModule,
-        HttpClientModule
+        HttpClientModule,
+        RouterModule
     ],
-    exports: []
+    providers: [
+        HttpClientService
+    ],
+    exports: [
+        HeaderComponent,
+        FooterComponent
+    ]
 })
 export class CoreModule {}
