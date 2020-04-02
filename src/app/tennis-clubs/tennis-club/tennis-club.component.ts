49eb7be6f921cce5440172543f0dbaf2df27697e
Georgi <GeorgiPanchev@mail.com>
2020-04-02 19:38:05 +0300

    Tennis Club Component Template Added

---
+++ src/app/tennis-clubs/tennis-club/tennis-club.component.ts
@@ -0,0 +1,7 @@
+import { Component, OnDestroy } from '@angular/core';
+
+@Component({
+    selector: 'app-tennis-club',
+    templateUrl: './tennis-club.component.html'
+})
+export class TennisClubComponent implements OnDestroy {}
