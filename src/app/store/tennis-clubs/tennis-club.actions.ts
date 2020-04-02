dbc36366a3321f408a800e71eae91dfd3d14d87e
Georgi <GeorgiPanchev@mail.com>
2020-04-02 18:49:46 +0300

    Store Tennis Club Actions Added

---
+++ src/app/store/tennis-clubs/tennis-club.actions.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from '../app.state';
+import { TennisClubService } from '../../tennis-clubs/shared/tennis-club.service';
+
+export const CREATE_TENNIS_CLUB = 'CREATE_TENNIS_CLUB';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class TennisClubActions {
+    // eslint-disable-next-line no-useless-constructor
+    constructor(
+        private store: Store<AppState>,
+        private tennisClubService: TennisClubService
+    ) {}
+
+    create(tennisClub) {
+        this.tennisClubService.create(tennisClub)
+            .subscribe((response) => {
+                this.store.dispatch({
+                    type: CREATE_TENNIS_CLUB,
+                    payload: response
+                });
+            });
+    }
+}
