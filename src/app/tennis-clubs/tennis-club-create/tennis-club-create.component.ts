8c15750ebbcaef3e698fd54b46d372990bd93ccd
Georgi <GeorgiPanchev@mail.com>
2020-04-02 19:07:16 +0300

    Tennis Club Create Template Added

---
+++ src/app/tennis-clubs/tennis-club-create/tennis-club-create.component.ts
@@ -0,0 +1,7 @@
+import { Component, OnDestroy } from '@angular/core';
+
+@Component({
+    selector: 'app-tennis-club-create',
+    templateUrl: './tennis-club-create.component.html'
+})
+export class TennisClubCreateComponent implements OnDestroy {}
--- src/app/tennis-clubs/tennis-clubs.module.ts
+++ src/app/tennis-clubs/tennis-clubs.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { TennisClubFormComponent } from './tennis-club-form/tennis-club-form.component';
+import { TennisClubCreateComponent } from './tennis-club-create/tennis-club-create.component';
 
 @NgModule({
     declarations: [
-        TennisClubFormComponent
+        TennisClubFormComponent,
+        TennisClubCreateComponent
     ],
     imports: [
         CommonModule,
