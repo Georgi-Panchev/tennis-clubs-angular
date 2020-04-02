67c52110f1c5a541b6a8358c2fc288d88982283f
Georgi <GeorgiPanchev@mail.com>
2020-04-02 19:48:26 +0300

    Tennis Club Component Logic Added

--- src/app/tennis-clubs/tennis-club/tennis-club.component.ts
+++ src/app/tennis-clubs/tennis-club/tennis-club.component.ts
@@ -1,7 +1,33 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component, Input, OnDestroy } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../store/app.state';
+import { Subscription } from 'rxjs';
+import { UserState } from '../../store/users/user.state';
 
 @Component({
     selector: 'app-tennis-club',
     templateUrl: './tennis-club.component.html'
 })
-export class TennisClubComponent implements OnDestroy {}
+export class TennisClubComponent implements OnDestroy {
+    @Input() tennisClub;
+    private subscription: Subscription = new Subscription();
+    userState = null;
+
+    constructor(
+        private store: Store<AppState>
+    ) {
+        this.subscription.add(
+            this.store
+                .select((state: AppState) => state.users)
+                .subscribe((userState: UserState) => (this.userState = userState))
+        );
+    }
+
+    removeClub(tennisClubId) {
+
+    }
+
+    ngOnDestroy(): void {
+        this.subscription.unsubscribe();
+    }
+}
--- src/app/tennis-clubs/tennis-clubs.module.ts
+++ src/app/tennis-clubs/tennis-clubs.module.ts
@@ -4,11 +4,13 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { TennisClubFormComponent } from './tennis-club-form/tennis-club-form.component';
 import { TennisClubCreateComponent } from './tennis-club-create/tennis-club-create.component';
+import { TennisClubComponent } from './tennis-club/tennis-club.component';
 
 @NgModule({
     declarations: [
         TennisClubFormComponent,
-        TennisClubCreateComponent
+        TennisClubCreateComponent,
+        TennisClubComponent
     ],
     imports: [
         CommonModule,
