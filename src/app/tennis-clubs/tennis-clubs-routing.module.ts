be9c71bcf31521155f83a0a1006df1a064851e74
Georgi <GeorgiPanchev@mail.com>
2020-04-03 19:19:15 +0300

    Tennis Clubs Routing Updated

--- src/app/tennis-clubs/tennis-clubs-routing.module.ts
+++ src/app/tennis-clubs/tennis-clubs-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { AuthUserActivate } from '../core/guards/auth-user-activate.guard';
 import { AuthAdminActivate } from '../core/guards/auth-admin-activate.guard';
 
 import { TennisClubListResolver } from './shared/tennis-club-list.resolver';
+import { TennisClubDetailsResolver } from './shared/tennis-club-details.resolver';
 
 import { TennisClubListComponent } from './tennis-club-list/tennis-club-list.component';
 import { TennisClubCreateComponent } from './tennis-club-create/tennis-club-create.component';
+import { TennisClubDetailsComponent } from './tennis-club-details/tennis-club-details.component';
 
 const routes: Routes = [
     {
@@ -23,6 +26,12 @@ const routes: Routes = [
         path: 'create',
         canActivate: [ AuthAdminActivate ],
         component: TennisClubCreateComponent
+    },
+    {
+        path: ':tennisClubId/details',
+        canActivate: [ AuthUserActivate ],
+        resolve: [ TennisClubDetailsResolver ],
+        component: TennisClubDetailsComponent
     }
 ];
 
