8c358be1f38b24c01cf8423c90b7ccafc82787ac
Georgi <GeorgiPanchev@mail.com>
2020-04-03 19:05:42 +0300

    Tennis Club Reducer Updated

--- src/app/store/tennis-clubs/tennis-club.reducers.ts
+++ src/app/store/tennis-clubs/tennis-club.reducers.ts
@@ -1,7 +1,8 @@
 import { initialState, TennisClubState } from './tennis-club.state';
 import {
     CREATE_TENNIS_CLUB,
-    READ_TENNIS_CLUB_LIST
+    READ_TENNIS_CLUB_LIST,
+    READ_ONE_TENNIS_CLUB
 } from './tennis-club.actions';
 
 function create(state, action) {
@@ -18,12 +19,21 @@ function read(state, action) {
     });
 }
 
+function readOne(state, action) {
+    const payload = action.payload;
+    return Object.assign({}, state, {
+        tennisClub: payload.club
+    });
+}
+
 export function tennisClubReducers(state: TennisClubState = initialState, action) {
     switch (action.type) {
         case CREATE_TENNIS_CLUB:
             return create(state, action);
         case READ_TENNIS_CLUB_LIST:
             return read(state, action);
+        case READ_ONE_TENNIS_CLUB:
+            return readOne(state, action);
         default:
             return state;
     }
