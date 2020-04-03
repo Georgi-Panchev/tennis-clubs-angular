9f94dedb1a26aff1a9775601f1ffbc96e359824e
Georgi <GeorgiPanchev@mail.com>
2020-04-03 19:03:57 +0300

    Tennis Club State Updated

--- src/app/store/tennis-clubs/tennis-club.state.ts
+++ src/app/store/tennis-clubs/tennis-club.state.ts
@@ -3,9 +3,11 @@ import { TennisClubModel } from '../../tennis-clubs/shared/tennis-club.model';
 export interface TennisClubState {
     isTennisClubCreated: boolean;
     tennisClubList: TennisClubModel[];
+    tennisClub: TennisClubModel;
 }
 
 export const initialState: TennisClubState = {
     isTennisClubCreated: false,
-    tennisClubList: []
+    tennisClubList: [],
+    tennisClub: null
 };
