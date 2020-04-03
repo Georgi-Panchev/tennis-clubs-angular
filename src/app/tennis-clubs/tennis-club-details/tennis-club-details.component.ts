ac19072e4df4f5c5f3dec7dc681da1d76c2a2817
Georgi <GeorgiPanchev@mail.com>
2020-04-03 19:16:25 +0300

    Tennis Club Details Logic Added

--- src/app/tennis-clubs/tennis-club-details/tennis-club-details.component.ts
+++ src/app/tennis-clubs/tennis-club-details/tennis-club-details.component.ts
@@ -1,10 +1,39 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../store/app.state';
+import { TennisClubModel } from '../shared/tennis-club.model';
 
 @Component({
     selector: 'app-tennis-club-details',
-    templateUrl: './tennis-club-details.component.html',
-    styleUrls: [ './tennis-club-details.component.css' ]
+    templateUrl: './tennis-club-details.component.html'
 })
 export class TennisClubDetailsComponent implements OnInit, OnDestroy {
+    private subscription: Subscription = new Subscription();
+    isUserAdmin = false;
+    tennisClub: TennisClubModel = null;
 
+    constructor(private store: Store<AppState>) {
+        this.subscription.add(
+            this.store
+                .select((state: AppState) => state.users.user)
+                .subscribe((user) => (
+                    this.isUserAdmin = user.roles.includes('Admin')
+                ))
+        );
+    }
+
+    ngOnInit(): void {
+        this.subscription.add(
+            this.store
+                .select((state: AppState) => state.tennisClubs.tennisClub)
+                .subscribe((tennisClub: TennisClubModel) => (
+                    this.tennisClub = tennisClub
+                ))
+        );
+    }
+
+    ngOnDestroy(): void {
+        this.subscription.unsubscribe();
+    }
 }
