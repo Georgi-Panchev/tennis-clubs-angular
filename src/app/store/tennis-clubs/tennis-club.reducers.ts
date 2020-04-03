fa8d3fd8833586642e5f31868f82ff3b184b0da8
Georgi <GeorgiPanchev@mail.com>
2020-04-03 19:24:20 +0300

    Store Tennis Club Reducers Updated

--- src/app/store/tennis-clubs/tennis-club.reducers.ts
+++ src/app/store/tennis-clubs/tennis-club.reducers.ts
@@ -2,7 +2,8 @@ import { initialState, TennisClubState } from './tennis-club.state';
 import {
     CREATE_TENNIS_CLUB,
     READ_TENNIS_CLUB_LIST,
-    READ_ONE_TENNIS_CLUB
+    READ_ONE_TENNIS_CLUB,
+    EDIT_TENNIS_CLUB
 } from './tennis-club.actions';
 
 function create(state, action) {
@@ -26,6 +27,13 @@ function readOne(state, action) {
     });
 }
 
+function edit(state, action) {
+    const payload = action.payload;
+    return Object.assign({}, state, {
+        isTennisClubEdited: payload.success
+    });
+}
+
 export function tennisClubReducers(state: TennisClubState = initialState, action) {
     switch (action.type) {
         case CREATE_TENNIS_CLUB:
@@ -34,6 +42,8 @@ export function tennisClubReducers(state: TennisClubState = initialState, action
             return read(state, action);
         case READ_ONE_TENNIS_CLUB:
             return readOne(state, action);
+        case EDIT_TENNIS_CLUB:
+            return edit(state, action);
         default:
             return state;
     }
