cd2c3c4a546c607ca9047c5752640073123f1019
Georgi <GeorgiPanchev@mail.com>
2020-04-04 12:05:02 +0300

    Store Tennis Tournament Actions Updated

--- src/app/store/tennis-tournaments/tennis-tournament.actions.ts
+++ src/app/store/tennis-tournaments/tennis-tournament.actions.ts
@@ -6,6 +6,7 @@ import { TennisTournamentService } from '../../tennis-tournaments/shared/tennis-
 export const CREATE_TENNIS_TOURNAMENT = 'CREATE_TENNIS_TOURNAMENT';
 export const READ_TENNIS_TOURNAMENT_LIST = 'READ_TENNIS_TOURNAMENT_LIST';
 export const READ_ONE_TENNIS_TOURNAMENT = 'READ_ONE_TENNIS_TOURNAMENT';
+export const EDIT_TENNIS_TOURNAMENT = 'EDIT_TENNIS_TOURNAMENT';
 
 @Injectable({
     providedIn: 'root'
@@ -22,6 +23,16 @@ export class TennisTournamentActions {
             .subscribe((response) => {
                 this.store.dispatch({
                     type: CREATE_TENNIS_TOURNAMENT,
+                    payload: response
+                });
+            });
+    }
+
+    edit(tennisTournamentId, tennisTournament) {
+        this.tennisTournamentService.edit(tennisTournamentId, tennisTournament)
+            .subscribe((response) => {
+                this.store.dispatch({
+                    type: EDIT_TENNIS_TOURNAMENT,
                     payload: response
                 });
             });
