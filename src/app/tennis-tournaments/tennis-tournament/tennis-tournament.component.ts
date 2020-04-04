480e846561817a5e04293886e8f1a61385a3a5d0
Georgi <GeorgiPanchev@mail.com>
2020-04-04 11:09:01 +0300

    Tennis Tournament Component Template Added

---
+++ src/app/tennis-tournaments/tennis-tournament/tennis-tournament.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnDestroy } from '@angular/core';
+
+@Component({
+    selector: 'app-tennis-tournament',
+    templateUrl: './tennis-tournament.component.html',
+    styleUrls: [ './tennis-tournament.component.css' ]
+})
+export class TennisTournamentComponent implements OnDestroy {
+
+}
