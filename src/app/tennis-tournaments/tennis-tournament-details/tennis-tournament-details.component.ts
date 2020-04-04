fe997b2e1ee38b90202d5bf71e81a6c162f461d9
Georgi <GeorgiPanchev@mail.com>
2020-04-04 13:41:15 +0300

    Tennis Tournament Details Component Updated

--- src/app/tennis-tournaments/tennis-tournament-details/tennis-tournament-details.component.ts
+++ src/app/tennis-tournaments/tennis-tournament-details/tennis-tournament-details.component.ts
@@ -4,6 +4,7 @@ import { Store } from '@ngrx/store';
 import { AppState } from '../../store/app.state';
 import { TennisTournamentModel } from '../shared/tennis-tournament.model';
 import { UserState } from '../../store/users/user.state';
+import { TennisTournamentActions } from '../../store/tennis-tournaments/tennis-tournament.actions';
 
 @Component({
     selector: 'app-tennis-tournament-details',
@@ -13,9 +14,11 @@ export class TennisTournamentDetailsComponent implements OnInit, OnDestroy {
     private subscription: Subscription = new Subscription();
     userState = null;
     tennisTournament: TennisTournamentModel = null;
+    isTournamentAttended = false;
 
     constructor(
-        private store: Store<AppState>
+        private store: Store<AppState>,
+        private tennisTournamentActions: TennisTournamentActions
     ) {
         this.subscription.add(
             this.store
@@ -30,12 +33,14 @@ export class TennisTournamentDetailsComponent implements OnInit, OnDestroy {
                 .select((state: AppState) => state.tennisTournaments.tennisTournament)
                 .subscribe((tennisTournament: TennisTournamentModel) => {
                     this.tennisTournament = tennisTournament;
+                    this.isTournamentAttended = this.tennisTournament.playersRegistered
+                        .some((user: any) => user._id === this.userState.user.userId);
                 })
         );
     }
 
     attendTournament(tennisTournamentId) {
-
+        this.tennisTournamentActions.attend(tennisTournamentId);
     }
 
     leaveTournament(tennisTournamentId) {
