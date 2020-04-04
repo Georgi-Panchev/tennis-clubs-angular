2975543f2c1cc5668755746fc56118c6f9272e6e
Georgi <GeorgiPanchev@mail.com>
2020-04-04 10:30:55 +0300

    Tennis Tournament Form Template Added

---
+++ src/app/tennis-tournaments/tennis-tournament-form/tennis-tournament-form.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-tennis-tournament-form',
+    templateUrl: './tennis-tournament-form.component.html',
+    styleUrls: [ './tennis-tournament-form.component.css' ]
+})
+export class TennisTournamentFormComponent implements OnInit {
+
+}
--- src/app/tennis-tournaments/tennis-tournaments.module.ts
+++ src/app/tennis-tournaments/tennis-tournaments.module.ts
@@ -2,8 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
+import { TennisTournamentFormComponent } from './tennis-tournament-form/tennis-tournament-form.component';
+
 @NgModule({
-    declarations: [],
+    declarations: [
+        TennisTournamentFormComponent
+    ],
     imports: [
         CommonModule,
         ReactiveFormsModule
