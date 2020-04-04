95926f7da7c37c7dc35fdfe1dc12223ad45c5b66
Georgi <GeorgiPanchev@mail.com>
2020-04-04 12:39:35 +0300

    Tennis Tournament List By Club Resolver Added

---
+++ src/app/tennis-tournaments/shared/tennis-tournament-list-by-club.resolver.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../store/app.state';
+import { Observable, of } from 'rxjs';
+import { tap, mapTo, catchError } from 'rxjs/operators';
+import { TennisTournamentService } from './tennis-tournament.service';
+import { READ_TENNIS_TOURNAMENT_LIST_BY_CLUB } from '../../store/tennis-tournaments/tennis-tournament.actions';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class TennisTournamentListByClubResolver implements Resolve<Observable<boolean>> {
+    // eslint-disable-next-line no-useless-constructor
+    constructor(
+        private tennisTournamentService: TennisTournamentService,
+        private store: Store<AppState>
+    ) {}
+
+    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
+        const tennisClubId = route.params.tennisClubId;
+        return this.tennisTournamentService.readByClub(tennisClubId)
+            .pipe(
+                tap((response) => {
+                    this.store.dispatch({
+                        type: READ_TENNIS_TOURNAMENT_LIST_BY_CLUB,
+                        payload: response
+                    });
+                }),
+                mapTo(true),
+                catchError((error) => of(error))
+            );
+    }
+}
