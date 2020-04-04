80296f8ee5426d2731f280a272c1a15d2aed0a91
Georgi <GeorgiPanchev@mail.com>
2020-04-04 12:02:13 +0300

    Tennis Tournament Details Logic Added

--- src/app/tennis-tournaments/tennis-tournament-details/tennis-tournament-details.component.ts
+++ src/app/tennis-tournaments/tennis-tournament-details/tennis-tournament-details.component.ts
@@ -3,15 +3,46 @@ import { Subscription } from 'rxjs';
 import { Store } from '@ngrx/store';
 import { AppState } from '../../store/app.state';
 import { TennisTournamentModel } from '../shared/tennis-tournament.model';
-import { TennisTournamentActions } from '../../store/tennis-tournaments/tennis-tournament.actions';
-import { Router } from '@angular/router';
 import { UserState } from '../../store/users/user.state';
 
 @Component({
     selector: 'app-tennis-tournament-details',
-    templateUrl: './tennis-tournament-details.component.html',
-    styleUrls: [ './tennis-tournament-details.component.css' ]
+    templateUrl: './tennis-tournament-details.component.html'
 })
 export class TennisTournamentDetailsComponent implements OnInit, OnDestroy {
+    private subscription: Subscription = new Subscription();
+    userState = null;
+    tennisTournament: TennisTournamentModel = null;
 
+    constructor(
+        private store: Store<AppState>
+    ) {
+        this.subscription.add(
+            this.store
+                .select((state: AppState) => state.users)
+                .subscribe((userState: UserState) => (this.userState = userState))
+        );
+    }
+
+    ngOnInit(): void {
+        this.subscription.add(
+            this.store
+                .select((state: AppState) => state.tennisTournaments.tennisTournament)
+                .subscribe((tennisTournament: TennisTournamentModel) => {
+                    this.tennisTournament = tennisTournament;
+                })
+        );
+    }
+
+    attendTournament(tennisTournamentId) {
+
+    }
+
+    leaveTournament(tennisTournamentId) {
+
+    }
+
+    ngOnDestroy(): void {
+        this.subscription.unsubscribe();
+    }
 }
--- src/app/tennis-tournaments/tennis-tournaments.module.ts
+++ src/app/tennis-tournaments/tennis-tournaments.module.ts
@@ -1,22 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import { TennisTournamentsRoutingModule } from './tennis-tournaments-routing.module';
 
 import { TennisTournamentFormComponent } from './tennis-tournament-form/tennis-tournament-form.component';
 import { TennisTournamentCreateComponent } from './tennis-tournament-create/tennis-tournament-create.component';
 import { TennisTournamentComponent } from './tennis-tournament/tennis-tournament.component';
 import { TennisTournamentListComponent } from './tennis-tournament-list/tennis-tournament-list.component';
+import { TennisTournamentDetailsComponent } from './tennis-tournament-details/tennis-tournament-details.component';
 
 @NgModule({
     declarations: [
         TennisTournamentFormComponent,
         TennisTournamentCreateComponent,
         TennisTournamentComponent,
-        TennisTournamentListComponent
+        TennisTournamentListComponent,
+        TennisTournamentDetailsComponent
     ],
     imports: [
         CommonModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        TennisTournamentsRoutingModule
     ],
     exports: []
 })
