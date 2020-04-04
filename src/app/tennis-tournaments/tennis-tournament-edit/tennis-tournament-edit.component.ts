432a2172536c1567e85bd8705a10b743bdb0297c
Georgi <GeorgiPanchev@mail.com>
2020-04-04 12:34:04 +0300

    Tennis Tournament Edit Logic Added

--- src/app/tennis-tournaments/tennis-tournament-edit/tennis-tournament-edit.component.ts
+++ src/app/tennis-tournaments/tennis-tournament-edit/tennis-tournament-edit.component.ts
@@ -8,9 +8,51 @@ import { TennisTournamentActions } from '../../store/tennis-tournaments/tennis-t
 
 @Component({
     selector: 'app-tennis-tournament-edit',
-    templateUrl: './tennis-tournament-edit.component.html',
-    styleUrls: [ './tennis-tournament-edit.component.css' ]
+    templateUrl: './tennis-tournament-edit.component.html'
 })
 export class TennisTournamentEditComponent implements OnInit, OnDestroy {
+    private subscription: Subscription = new Subscription();
+    tennisTournament: TennisTournamentModel = null;
 
+    // eslint-disable-next-line no-useless-constructor
+    constructor(
+        private router: Router,
+        private activatedRoute: ActivatedRoute,
+        private store: Store<AppState>,
+        private tennisTournamentActions: TennisTournamentActions
+    ) {}
+
+    ngOnInit(): void {
+        this.subscription.add(
+            this.store
+                .select((state: AppState) => state.tennisTournaments.tennisTournament)
+                .subscribe((tennisTournament: TennisTournamentModel) => {
+                    this.tennisTournament = tennisTournament;
+                })
+        );
+    }
+
+    edit(tennisTournament) {
+        console.log(tennisTournament);
+        this.subscription.add(
+            this.activatedRoute.params
+                .subscribe((params: Params) => (
+                    this.tennisTournamentActions.edit(params.tennisTournamentId, tennisTournament)
+                ))
+        );
+
+        this.subscription.add(
+            this.store
+                .select((state: AppState) => state.tennisTournaments.isTennisTournamentEdited)
+                .subscribe((isTennisTournamentEdited: boolean) => {
+                    if (isTennisTournamentEdited) {
+                        this.router.navigateByUrl('tennis-tournaments/list');
+                    }
+                })
+        );
+    }
+
+    ngOnDestroy(): void {
+        this.subscription.unsubscribe();
+    }
 }
