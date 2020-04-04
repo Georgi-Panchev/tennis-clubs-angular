3712a85213588a48dfdca449497a5c2e9c5139b7
Georgi <GeorgiPanchev@mail.com>
2020-04-04 11:17:00 +0300

    Tennis Tournament Component Logic Added

--- src/app/tennis-tournaments/tennis-tournament/tennis-tournament.component.ts
+++ src/app/tennis-tournaments/tennis-tournament/tennis-tournament.component.ts
@@ -1,10 +1,33 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component, Input, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../store/app.state';
+import { UserState } from '../../store/users/user.state';
 
 @Component({
     selector: 'app-tennis-tournament',
-    templateUrl: './tennis-tournament.component.html',
-    styleUrls: [ './tennis-tournament.component.css' ]
+    templateUrl: './tennis-tournament.component.html'
 })
 export class TennisTournamentComponent implements OnDestroy {
+    @Input() tennisTournament;
+    private subscription: Subscription = new Subscription();
+    userState = null;
 
+    constructor(
+        private store: Store<AppState>
+    ) {
+        this.subscription.add(
+            this.store
+                .select((state: AppState) => state.users)
+                .subscribe((userState: UserState) => (this.userState = userState))
+        );
+    }
+
+    removeTournament(tennisTournamentId) {
+
+    }
+
+    ngOnDestroy(): void {
+        this.subscription.unsubscribe();
+    }
 }
--- src/app/tennis-tournaments/tennis-tournaments.module.ts
+++ src/app/tennis-tournaments/tennis-tournaments.module.ts
@@ -4,11 +4,13 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { TennisTournamentFormComponent } from './tennis-tournament-form/tennis-tournament-form.component';
 import { TennisTournamentCreateComponent } from './tennis-tournament-create/tennis-tournament-create.component';
+import { TennisTournamentComponent } from './tennis-tournament/tennis-tournament.component';
 
 @NgModule({
     declarations: [
         TennisTournamentFormComponent,
-        TennisTournamentCreateComponent
+        TennisTournamentCreateComponent,
+        TennisTournamentComponent
     ],
     imports: [
         CommonModule,
